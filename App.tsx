2536edd0921c8e2907edfcd1a9fc0bdc2b4455eb
yardenho <hovav4321@gmail.com>
2023-01-05 20:40:51 +0200

    adding brick

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FC } from "react";
+import {
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 
-export default function App() {
+const Brick: FC = () => {
     return (
-        <View style={styles.container}>
-            <Text>X Mix Drix</Text>
-            <StatusBar style="auto" />
+        <View style={styles.brick}>
+            <TouchableOpacity></TouchableOpacity>
         </View>
     );
-}
+};
+
+const App: FC = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.row}></View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
+        marginTop: StatusBar.currentHeight,
         flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+    },
+    row: {
+        flexDirection: "row",
+        backgroundColor: "blue",
+    },
+    brick: {
+        flex: 1,
+        backgroundColor: "white",
+        margin: 5,
+        aspectRatio: 1,
     },
 });
