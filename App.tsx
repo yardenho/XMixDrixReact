da0f87e5d57ad271705ea29e179b51e9a868ba6b
yardenho <hovav4321@gmail.com>
2023-01-15 23:10:08 +0200

    done

--- App.tsx
+++ App.tsx
@@ -13,16 +13,17 @@ const Brick: FC<{
     brickNumber: number;
     onClick: (brickNUm: number) => void;
     getCurrentPlayer: () => number;
+    cells: number[];
+    winner: number;
 }> = (props) => {
     const [player, setPlayer] = useState(0);
-
     const onBrickClick = () => {
         setPlayer(props.getCurrentPlayer());
         props.onClick(props.brickNumber);
     };
 
     const player1 = () => {
-        if (player == 1) {
+        if (props.cells[props.brickNumber] == 1) {
             return "flex";
         } else {
             return "none";
@@ -30,20 +31,26 @@ const Brick: FC<{
     };
 
     const player2 = () => {
-        if (player == 2) {
+        if (props.cells[props.brickNumber] == 2) {
             return "flex";
         } else {
             return "none";
         }
     };
-    console.log(player);
+
+    const disableBrick = () => {
+        if (props.winner != 0) {
+            return true;
+        }
+        return props.cells[props.brickNumber] ? true : false;
+    };
 
     return (
         <View style={styles.brick}>
             <TouchableOpacity
                 style={[styles.button]}
                 onPress={onBrickClick}
-                disabled={player ? true : false}
+                disabled={disableBrick()}
             >
                 <Image
                     style={[styles.image, { display: player1() }]}
@@ -58,33 +65,39 @@ const Brick: FC<{
     );
 };
 
-const BrickRow: FC<{
-    brickRowNumber: number;
-    onClick: (brickNUm: number) => void;
-    getCurrentPlayer: () => number;
-    linesInfo: () => number[];
-}> = (props) => {
-    const horizontalDisplay = () => {
-        const [topHorizontal, middleHorizontal, bottomHorizontal] =
-            props.linesInfo();
-        console.log("topHorizontal " + topHorizontal);
-        console.log("middleHorizontal " + middleHorizontal);
-        console.log("bottomHorizontal " + bottomHorizontal);
+const App: FC = () => {
+    // 0: not selected, 1: 'x', 2: 'o'
+    const [turn, setTurn] = useState(1);
+    const [Cells, setCells] = useState(Array(9).fill(0));
+    const [topHorizontal, setTopHorizontal] = useState(0);
+    const [middleHorizontal, setMiddleHorizontal] = useState(0);
+    const [bottomHorizontal, setBottomHorizontal] = useState(0);
+    const [leftVertical, setLeftVertical] = useState(0);
+    const [middleVertical, setMiddleVertical] = useState(0);
+    const [rightVertical, setRightVertical] = useState(0);
+    const [mainDiagonal, setMainDiagonal] = useState(0);
+    const [seconderyDiagonal, setSeconderyDiagonal] = useState(0);
+    const [winner, setWinner] = useState(0);
 
+    const getCurrentPlayer = () => {
+        return turn;
+    };
+
+    const horizontalDisplay = (rowNumber: number) => {
         if (topHorizontal == 1) {
-            if (props.brickRowNumber == 1) {
+            if (rowNumber == 1) {
                 return "flex";
             } else {
                 return "none";
             }
         } else if (middleHorizontal == 1) {
-            if (props.brickRowNumber == 4) {
+            if (rowNumber == 2) {
                 return "flex";
             } else {
                 return "none";
             }
         } else if (bottomHorizontal == 1) {
-            if (props.brickRowNumber == 7) {
+            if (rowNumber == 3) {
                 return "flex";
             } else {
                 return "none";
@@ -94,78 +107,28 @@ const BrickRow: FC<{
         }
     };
 
-    return (
-        <View style={styles.row}>
-            <Brick
-                brickNumber={props.brickRowNumber}
-                onClick={props.onClick}
-                getCurrentPlayer={props.getCurrentPlayer}
-            ></Brick>
-            <Brick
-                brickNumber={props.brickRowNumber + 1}
-                onClick={props.onClick}
-                getCurrentPlayer={props.getCurrentPlayer}
-            ></Brick>
-            <Brick
-                brickNumber={props.brickRowNumber + 2}
-                onClick={props.onClick}
-                getCurrentPlayer={props.getCurrentPlayer}
-            ></Brick>
-            <Image
-                style={[styles.imageLines, { display: horizontalDisplay() }]}
-                source={require("./assets/horizontal.png")}
-            />
-        </View>
-    );
-};
-
-const App: FC = () => {
-    // 0: not selected, 1: 'x', 2: 'o'
-    var turn = 1;
-    var oCells: number[] = [];
-    var xCells: number[] = [];
-    const [topHorizontal, setTopHorizontal] = useState(0);
-    const [middleHorizontal, setMiddleHorizontal] = useState(0);
-    const [bottomHorizontal, setBottomHorizontal] = useState(0);
-    const [leftVertical, setLeftVertical] = useState(0);
-    const [middleVertical, setMiddleVertical] = useState(0);
-    const [rightVertical, setRightVertical] = useState(0);
-    const [mainDiagonal, setMainDiagonal] = useState(0);
-    const [seconderyDiagonal, setSeconderyDiagonal] = useState(0);
-    const [newGameClicked, setNewGameClicked] = useState(0);
-
-    const getCurrentPlayer = () => {
-        return turn;
-    };
-
-    const getLinesInfo = () => {
-        return [topHorizontal, middleHorizontal, bottomHorizontal];
-    };
-
     const updateCells = (brickNum: number) => {
-        if (turn == 1) {
-            xCells.push(brickNum);
-        } else if (turn == 2) {
-            oCells.push(brickNum);
-        }
+        Cells[brickNum] = turn;
+
+        setCells(Cells);
     };
 
     const onBrickClick = (brickNumber: number) => {
-        console.log("onBrickClick");
-        if (turn == 1) {
-            turn = 2;
-        } else {
-            turn = 1;
-        }
         updateCells(brickNumber);
+
+        if (turn == 1) {
+            setTurn(2);
+        } else {
+            setTurn(1);
+        }
         checkWinner();
     };
 
     const verticalDisplay = (vertical: string) => {
-        console.log("in display");
-        console.log("leftVertical" + leftVertical);
-        console.log("middleVertical" + middleVertical);
-        console.log("rightVertical" + rightVertical);
+        console.log("leftVertical = " + leftVertical);
+        console.log("middleVertical = " + middleVertical);
+
+        console.log("rightVertical = " + rightVertical);
 
         if (leftVertical == 1 && vertical == "left") {
             return "flex";
@@ -179,7 +142,9 @@ const App: FC = () => {
     };
 
     const diagonalDisplay = (diagonal: String) => {
-        console.log(diagonal);
+        console.log("mainDiagonal = " + mainDiagonal);
+
+        console.log("seconderyDiagonal = " + seconderyDiagonal);
 
         if (mainDiagonal == 1 && diagonal == "toRight") {
             return "flex";
@@ -190,108 +155,86 @@ const App: FC = () => {
         }
     };
 
-    const checkVertical = (arr: number[]) => {
-        // 1, 4, 7
-        if (
-            arr.indexOf(1) != -1 &&
-            arr.indexOf(4) != -1 &&
-            arr.indexOf(7) != -1
-        ) {
+    const checkVertical = () => {
+        // 0, 3, 6
+        if (Cells[0] && Cells[0] == Cells[3] && Cells[3] == Cells[6]) {
             //win - to place the left vertical
             setLeftVertical(1);
+            setWinner(Cells[0]);
+        }
+        //1, 4, 7
+        if (Cells[1] && Cells[1] == Cells[4] && Cells[4] == Cells[7]) {
+            //win - to place the middel vertical
+            setMiddleVertical(1);
+            setWinner(Cells[1]);
         }
         //2, 5, 8
-        if (
-            arr.indexOf(2) != -1 &&
-            arr.indexOf(5) != -1 &&
-            arr.indexOf(8) != -1
-        ) {
-            //win - to place the middel vertical
-
-            setMiddleVertical(1);
-        }
-        //3, 6, 9
-        if (
-            arr.indexOf(3) != -1 &&
-            arr.indexOf(6) != -1 &&
-            arr.indexOf(9) != -1
-        ) {
+        if (Cells[2] && Cells[2] == Cells[5] && Cells[5] == Cells[8]) {
             //win - to place the right vertical
             setRightVertical(1);
+            setWinner(Cells[2]);
         }
     };
 
-    const checkHorizontal = (arr: number[]) => {
-        //1, 2, 3
-        if (
-            arr.indexOf(1) != -1 &&
-            arr.indexOf(2) != -1 &&
-            arr.indexOf(3) != -1
-        ) {
+    const checkHorizontal = () => {
+        //0, 1, 2
+        if (Cells[0] && Cells[0] == Cells[1] && Cells[1] == Cells[2]) {
             //win - to place the top herizontal
             setTopHorizontal(1);
+            setWinner(Cells[0]);
         }
-        //4, 5, 6
-        if (
-            arr.indexOf(4) != -1 &&
-            arr.indexOf(5) != -1 &&
-            arr.indexOf(6) != -1
-        ) {
+        //3, 4, 5
+        if (Cells[3] && Cells[3] == Cells[4] && Cells[4] == Cells[5]) {
             //win - to place the middel herizontal
             setMiddleHorizontal(1);
+            setWinner(Cells[3]);
         }
-        //7, 8, 9
-        if (
-            arr.indexOf(7) != -1 &&
-            arr.indexOf(8) != -1 &&
-            arr.indexOf(9) != -1
-        ) {
+        //6, 7, 8
+        if (Cells[6] && Cells[6] == Cells[7] && Cells[7] == Cells[8]) {
             //win - to place the bottom herizontal
             setBottomHorizontal(1);
+            setWinner(Cells[6]);
         }
     };
 
-    const checkDiagonals = (arr: number[]) => {
-        //1, 5, 9
-        if (
-            arr.indexOf(1) != -1 &&
-            arr.indexOf(5) != -1 &&
-            arr.indexOf(9) != -1
-        ) {
+    const checkDiagonals = () => {
+        //0, 4, 8
+        if (Cells[0] && Cells[0] == Cells[4] && Cells[4] == Cells[8]) {
             //win - to place the main diagonal
             setMainDiagonal(1);
+            setWinner(Cells[0]);
         }
-        //3, 5, 7
-        if (
-            arr.indexOf(3) != -1 &&
-            arr.indexOf(5) != -1 &&
-            arr.indexOf(7) != -1
-        ) {
+        //2, 4, 6
+        if (Cells[2] && Cells[2] == Cells[4] && Cells[4] == Cells[6]) {
             //win - to place the secondery diagonal
             setSeconderyDiagonal(1);
+            setWinner(Cells[2]);
         }
+    };
+
+    const checkNoWinner = () => {
+        for (var i = 0; i < Cells.length; ++i) {
+            if (Cells[i] == 0) {
+                return;
+            }
+        }
+        setWinner(-1);
     };
 
     const checkWinner = () => {
-        if (turn == 1) {
-            checkVertical(xCells);
-            checkHorizontal(xCells);
-            checkDiagonals(xCells);
-        } else if (turn == 2) {
-            checkVertical(oCells);
-            checkHorizontal(oCells);
-            checkDiagonals(oCells);
-        }
+        console.log(Cells);
+        checkVertical();
+        checkHorizontal();
+        checkDiagonals();
+        checkNoWinner();
     };
 
     console.log("My app is running");
 
     const newGame = () => {
-        setNewGameClicked(1);
         console.log("newGame was pressed");
-        turn = 1;
-        oCells = [];
-        xCells = [];
+        setCells(Array(9).fill(0));
+        setTurn(1);
         setTopHorizontal(0);
         setMiddleHorizontal(0);
         setBottomHorizontal(0);
@@ -300,46 +243,119 @@ const App: FC = () => {
         setRightVertical(0);
         setMainDiagonal(0);
         setSeconderyDiagonal(0);
+        setWinner(0);
     };
 
     return (
         <View style={styles.container}>
-            <BrickRow
-                brickRowNumber={1}
-                onClick={onBrickClick}
-                getCurrentPlayer={getCurrentPlayer}
-                linesInfo={getLinesInfo}
-            ></BrickRow>
-            <BrickRow
-                brickRowNumber={4}
-                onClick={onBrickClick}
-                getCurrentPlayer={getCurrentPlayer}
-                linesInfo={getLinesInfo}
-            ></BrickRow>
-            <BrickRow
-                brickRowNumber={7}
-                onClick={onBrickClick}
-                getCurrentPlayer={getCurrentPlayer}
-                linesInfo={getLinesInfo}
-            ></BrickRow>
+            <View style={styles.row}>
+                <Brick
+                    brickNumber={0}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Brick
+                    brickNumber={1}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Brick
+                    brickNumber={2}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Image
+                    style={[
+                        styles.imageLines,
+                        { display: horizontalDisplay(1) },
+                    ]}
+                    source={require("./assets/horizontal.png")}
+                />
+            </View>
+            <View style={styles.row}>
+                <Brick
+                    brickNumber={3}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Brick
+                    brickNumber={4}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Brick
+                    brickNumber={5}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Image
+                    style={[
+                        styles.imageLines,
+                        { display: horizontalDisplay(2), marginTop: 10 },
+                    ]}
+                    source={require("./assets/horizontal.png")}
+                />
+            </View>
+            <View style={styles.row}>
+                <Brick
+                    brickNumber={6}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Brick
+                    brickNumber={7}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Brick
+                    brickNumber={8}
+                    onClick={onBrickClick}
+                    getCurrentPlayer={getCurrentPlayer}
+                    cells={Cells}
+                    winner={winner}
+                ></Brick>
+                <Image
+                    style={[
+                        styles.imageLines,
+                        { display: horizontalDisplay(3) },
+                    ]}
+                    source={require("./assets/horizontal.png")}
+                />
+            </View>
             <Image
                 style={[
                     styles.imageLines,
-                    { display: verticalDisplay("left") },
+                    { display: verticalDisplay("left"), marginLeft: 20 },
                 ]}
                 source={require("./assets/vertical.png")}
             />
             <Image
                 style={[
                     styles.imageLines,
-                    { display: verticalDisplay("middle"), marginLeft: 150 },
+                    { display: verticalDisplay("middle"), marginLeft: 160 },
                 ]}
                 source={require("./assets/vertical.png")}
             />
             <Image
                 style={[
                     styles.imageLines,
-                    { display: verticalDisplay("right"), marginLeft: 300 },
+                    { display: verticalDisplay("right"), marginLeft: 290 },
                 ]}
                 source={require("./assets/vertical.png")}
             />
@@ -360,14 +376,32 @@ const App: FC = () => {
                     {
                         display: diagonalDisplay("toRight"),
                         transform: [{ rotate: "-45deg" }],
-                        marginLeft: 140,
+                        marginLeft: 170,
                     },
                 ]}
                 source={require("./assets/vertical.png")}
             />
 
-            <Text>Its the turn of player number {turn}</Text>
-            <Button onPress={newGame} title=" New Game !"></Button>
+            <Text style={styles.textBox}>
+                {winner === -1
+                    ? "No winner :("
+                    : winner != 0
+                    ? "The winner is " + winner + " !!!"
+                    : "Its the turn of player number " + turn}
+            </Text>
+            <TouchableOpacity onPress={newGame} style={styles.newGameButton}>
+                <Text
+                    style={{
+                        color: "white",
+                        backgroundColor: "dodgerblue",
+                        fontSize: 25,
+                        textAlign: "center",
+                        borderRadius: 6,
+                    }}
+                >
+                    New Game !
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -376,7 +410,7 @@ const styles = StyleSheet.create({
     container: {
         marginTop: StatusBar.currentHeight,
         flex: 1,
-        backgroundColor: "gray",
+        backgroundColor: "black",
     },
     row: {
         flexDirection: "row",
@@ -404,6 +438,19 @@ const styles = StyleSheet.create({
     containerColumn: {
         flex: 1,
         flexDirection: "column",
+    },
+    textBox: {
+        backgroundColor: "grey",
+        padding: 10,
+        textAlign: "center",
+        margin: 5,
+        fontSize: 18,
+        marginBottom: 20,
+    },
+    newGameButton: {
+        flex: 1,
+        margin: 5,
+        padding: 20,
     },
 });
 
